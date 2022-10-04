d86228dbe3e4ccf0c03b18c748a362c0a56f9432
KDunny <91023687+KDunny@users.noreply.github.com>
2022-10-05 00:15:47 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+# Every Frame In Order Twitter Bot
+
+A Python bot to post every frame of a TV show in order to Twitter with a set interval between tweets
+
+I would like to thank [Every SpongeBob Frame In Order](https://twitter.com/SbFramesInOrder) for the inspiration.
+
+# Requirements
+
+- Computer that will run 24/7 - I run this bot through Linux and even though I briefly tested it on Windows, I can't guarantee long-term Windows compatibility
+- Frames from your chosen TV show in `jpg` format - I recommend [ffmpeg](https://ffmpeg.org/) to extract frames from episodes
+- `tweepy` installed with pip - `pip install -U tweepy`
+- `discord-webhook` installed with pip - `pip install -U discord-webhook` 
+- Twitter account signed up on the Twitter Developer Portal with generated API key and secret, access token and secret with write permissions
+- Discord account with admin privileges to a server to [create a webhook to receive bot error notifications](https://support.discord.com/hc/en-us/articles/228383668)
+
+# How to use
+
+1. Download `main.py` and `allEPs.txt` from this repo
+2. Create a folder to store both of these files, this folder will then store the `frames` folder, `log.txt` and `config.json` after they have been generated.
+3. Run `main.py` once to generate `config.json` and the `frames` folder, the `frames` folder will be used to store frames from your chosen TV show. If you want to use a different name for the frames folder, you can edit the `frame_dir` variable on line 22 in `main.py`.
+
+Your bot folder should now look like this
+
+<img src="https://raw.githubusercontent.com/KDunny/every-frame-in-order-bot/main/Root%20folder%20structure.png"/>
+
+4. Edit `allEPs.txt` so it contains each season and episode number based on the frame folder structure on each line just like the example inside the file. Ensure you follow the example and enter an additional line at the end of the file with any text inside it, in the example I wrote `End`, this is needed so the bot picks up the actual last episode of your show. For example, for my instance of the bot I post frames of the show South Park which currently has 319 episodes and a movie that my bot will also post so my local `allEPs.txt` contains 321 lines, one more than the number of episodes including the movie. This method of storing the season and episode names means you **don't** have to have every future episode stored even though the bot may be months away from needing it and allows you to add episode frame filled folders while the bot is running and can delete episode folders after the bot has posted them, this is useful for longer shows where you don't want to store hundreds of episodes as frames at a time.
+5. Edit `config.json` with your generated Twitter API key and secret, access token and secret ensuring write permissions were enabled for the access token and secret. Also enter your [Discord webhook URL](https://support.discord.com/hc/en-us/articles/228383668), [Discord user ID](https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-), TV show name and delay between tweets in seconds.
+6. Extract frames from your chosen TV show with [ffmpeg](https://ffmpeg.org/) and store them inside your `frames` folder, each season with a dedicated folder and each episode with a dedicated folder inside its associated season folder, in your ffmpeg command ensure the file output is `frame-%d.jpg`, this will extract frames in order named as `frame-1.jpg`,`frame-2.jpg`, etc. After extraction, your frames should be stored like this example `frames/Season 1/Episode 1/frame-1.jpg`.
+7. Finally you can run `main.py` and the bot should start posting frames to your Twitter!
+
+# Accounts running this bot
+Please contact me through the [@SPFramesInOrder](https://twitter.com/SPFramesInOrder) Twitter DM's if you are using this script for your Twitter bot, I would love to add your account to this list!
+
+<a href="https://twitter.com/SPFramesInOrder">
+<img src="https://pbs.twimg.com/profile_images/1554072503227645952/t-IIpZar_200x200.jpg" alt="logo" align="left" height="80"/>
+</a>
+
+Show: **South Park**
+
+Twitter: [**@SPFramesInOrder**](https://twitter.com/SPFramesInOrder)
---
+++ main.py
@@ -0,0 +1,331 @@
+from time import sleep
+from datetime import timedelta
+import socket
+import os
+import sys
+import logging
+import json
+import traceback
+import tweepy
+from discord_webhook import DiscordWebhook
+
+#Store the working directory
+dir = os.path.abspath(os.path.dirname(__file__))
+
+#Begin logging
+logging.basicConfig(filename="{dir}/log.txt".format(dir=dir), level=logging.INFO)
+
+print("Every Frame In Order Twitter Bot\n")
+logging.info("Every Frame In Order Twitter Bot\n")
+
+#Set frame directory name and check it exists, if not create it
+frame_dir = "frames"
+check_frame_dir = os.path.isdir("{dir}/{frame_dir}".format(dir=dir, frame_dir=frame_dir))
+if not check_frame_dir:
+    os.makedirs("{dir}/{frame_dir}".format(dir=dir, frame_dir=frame_dir))
+    print("Created frame folder: {dir}/{frame_dir} \n".format(dir=dir, frame_dir=frame_dir))
+    logging.info("Created frame folder: {dir}/{frame_dir} \n".format(dir=dir, frame_dir=frame_dir))
+
+#Load configuration from file
+config_file = 'config.json'
+config_path = '{dir}/{config_file}'.format(dir=dir, config_file=config_file)
+config_data = {
+    'api_key': 'YOUR_TWITTER_API_KEY_HERE',  # Twitter API Key
+    'api_secret': 'YOUR_TWITTER_API_SECRET_HERE',  # Twitter API Secret
+    'access_token': 'YOUR_TWITTER_ACCESS_TOKEN_HERE',  # Twitter API Access Token
+    'access_secret': 'YOUR_TWITTER_ACCESS_SECRET_HERE',  # Twitter API Access Secret
+    'webhookURL': 'YOUR_DISCORD_WEBHOOK_URL_HERE', # Discord Webhook URL for alerts
+    'userID': 'YOUR_DISCORD_USER_ID_HERE', # Discord user ID so the bot knows who to tag, read README on GitHub to find your ID
+    'showName': 'NAME OF TV SHOW HERE', # Name of TV Show for use in tweet text
+    'tweetDelay': '180', # Number of seconds between each tweet
+    'currentEPnum': '1', # Current episode number, will be updated by bot
+    'currentEP': '', # Current season and episode in plain text, will be updated by bot
+    'currentFrame': '1', # Current frame, will be updated by bot
+}
+
+def load_config():
+    global config_path, config_data
+    try:
+        #Try to load config data from file
+        with open(config_path) as f:
+            config_data = json.load(f)
+    except ValueError:
+        #If config file exists but it's malformed, add a flag
+        config_data['malformed'] = True
+    except Exception:
+        #If config file doesn't exist, create it and exit bot
+        with open(config_path, 'w') as f:
+            f.write(json.dumps(config_data, indent=4, sort_keys=False))
+            print("Please configure config located at {config_path}".format(config_path=config_path))
+            logging.info("Please configure config located at {config_path}".format(config_path=config_path))
+            sys.exit(1)
+
+    #Check file for if any fields are missing, if so add malformed flag
+    if any(item not in config_data for item in ['api_key', 'api_secret', 'access_token', 'access_secret', 'webhookURL', 'userID', 'showName', 'tweetDelay', 'currentEPnum', 'currentEP', 'currentFrame']):
+        config_data['malformed'] = True
+
+load_config()
+
+#Twitter access tokens
+api_key = config_data['api_key']
+api_secret = config_data['api_secret']
+access_token = config_data['access_token']
+access_secret = config_data['access_secret']
+
+#Authenticating with Twitter
+auth = tweepy.OAuth1UserHandler(api_key,api_secret,access_token,access_secret) 
+api = tweepy.API(auth, wait_on_rate_limit=True, retry_count=3, retry_delay=60, retry_errors=set([503]))
+client = tweepy.Client(consumer_key=api_key, consumer_secret=api_secret, access_token=access_token, access_token_secret=access_secret)
+
+#Discord webhook for alerts
+webhookURL = config_data['webhookURL']
+userID = config_data['userID']
+
+#TV show name
+showName = config_data['showName']
+print("TV Show:", showName)
+logging.info("TV Show: {showName}".format(showName=showName))
+
+#Number of seconds between tweets
+tweetDelay = int(config_data['tweetDelay'])
+
+#Store number of all episodes, current episode and then place in season_slash_episode which can be used to pull the current Season and Episode
+filer1 = open("{dir}/allEPs.txt".format(dir=dir),"r")
+allEPs = filer1.readlines()
+allEPs_lines = len(allEPs)
+filer1.close()
+currentEPnum = int(config_data['currentEPnum'])
+totalEPs = allEPs_lines - 1
+print("Total episodes:", totalEPs)
+logging.info("Total episodes: {totalEPs}".format(totalEPs=totalEPs))
+
+#Time remaining conversion from seconds
+def get_time_remaining(sec):
+    #Create timedelta and convert to string
+    timedelta_str = str(timedelta(seconds=sec))
+    #Split string into individual component
+    x = timedelta_str.split(':')
+    dh = x[0]
+    m = x[1]
+    s = x[2]
+    print("Frames remaining in episode: {remainingFrames} - Time remaining: {dh} hours {m} minutes {s} seconds".format(remainingFrames=remainingFrames, dh=dh, m=m, s=s))
+    logging.info("Frames remaining in episode: {remainingFrames} - Time remaining: {dh} hours {m} minutes {s} seconds".format(remainingFrames=remainingFrames, dh=dh, m=m, s=s))
+
+#Store traceback exception and print to console and log
+def print_error():
+    error = traceback.format_exc()
+    print(error)
+    logging.exception("An exception was thrown and the bot might have stopped posting!")
+    print("An error has occurred and the bot might have stopped posting!")
+
+#Discord webhook to inform about error, second message can fail if error is longer than maximum Discord message character count of 2000
+def send_discord_webhook():
+    webhook = DiscordWebhook(url=webhookURL, content='<@{userID}> An error has occurred with the Twitter bot and it might have stopped posting!'.format(userID=userID))
+    response = webhook.execute()
+    sleep(2)
+    error = traceback.format_exc()
+    webhook = DiscordWebhook(url=webhookURL, content='{error}'.format(error=error))
+    response = webhook.execute()
+    sleep(2)
+    webhook = DiscordWebhook(url=webhookURL, content='Error occurred on frame: {currentFrame}'.format(currentFrame=currentFrame))
+    response = webhook.execute()
+
+for j in range (currentEPnum, allEPs_lines):
+    while True:
+        try:
+            #Writes season_episode for use in tweet text
+            temp_season_slash_episode = allEPs[currentEPnum-1]
+            season_slash_episode = temp_season_slash_episode.replace("\n", "")
+            season_episode = season_slash_episode.replace("/", " ")
+            print("Current episode: {currentEPnum} - {season_episode}".format(currentEPnum=currentEPnum, season_episode=season_episode))
+            logging.info("Current episode: {currentEPnum} - {season_episode}".format(currentEPnum=currentEPnum, season_episode=season_episode))
+    
+            #Next episode variable
+            temp_nextEP = allEPs[currentEPnum]
+            nextEP_slash = temp_nextEP.replace("\n", "")
+            nextEP = nextEP_slash.replace("/", " ")
+            print("Next episode: {nextEP}".format(nextEP=nextEP))
+            logging.info("Next episode: {nextEP}".format(nextEP=nextEP))
+    
+            #Counts number of frames in folder and store it in a variable
+            frame_path = "{dir}/{frame_dir}/{season_slash_episode}".format(dir=dir, frame_dir=frame_dir, season_slash_episode=season_slash_episode)
+            totalFrames = int(0)
+            for path in os.listdir(frame_path):
+                if os.path.isfile(os.path.join(frame_path, path)):
+                    totalFrames += 1
+            print('Total number of frames:', totalFrames)
+            logging.info("Total number of frames: {totalFrames}".format(totalFrames=totalFrames))
+
+            #Resume from current frame
+            currentFrame = int(config_data['currentFrame'])
+            print("Current frame: ", currentFrame, "\n")
+            logging.info("Current frame: {currentFrame}".format(currentFrame=currentFrame))
+
+            for i in range (currentFrame, totalFrames+1):
+                while True:
+                    try:
+                        #Path to image that will be uploaded
+                        image_path = ("{frame_path}/frame-{i}.jpg".format(frame_path=frame_path, i=i))
+                        
+                        #Uploading image of frame to Twitter
+                        print("Uploading image of frame {i} to Twitter".format(i=i))
+                        media = api.media_upload(image_path)
+                        sleep(1)
+                        
+                        #Tweet text
+                        full_tweet = "{showName} - {season_episode} - Frame {i} out of {totalFrames}".format(showName=showName, season_episode=season_episode, i=i, totalFrames=totalFrames)
+                        
+                        #Send tweet with uploaded image and text
+                        print("Sending Tweet with frame {i} out of {totalFrames}".format(i=i, totalFrames=totalFrames))
+                        logging.info("Sending Tweet with frame {i} out of {totalFrames}".format(i=i, totalFrames=totalFrames))
+                        client.create_tweet(text=full_tweet, media_ids=[media.media_id])
+                        
+                        #Calculate remaining frames left in episode
+                        remainingFrames = totalFrames - currentFrame
+                        
+                        #Save current frame progress to config
+                        print("Saving progress to config")
+                        currentFrame = i + 1
+                        config_data['currentFrame'] = '{currentFrame}'.format(currentFrame=currentFrame)
+                        with open(config_path, 'w') as f:
+                            f.write(json.dumps(config_data, indent=4, sort_keys=False))
+                        
+                        #Calculate total seconds left until the episode finishes so it can be used in the conversion defined above
+                        secondsleft = remainingFrames * tweetDelay
+                        get_time_remaining(secondsleft)
+                        
+                        #If statement to inform through Discord that the episode has ended and that the next episode is about to start
+                        if remainingFrames == 0:
+                            print("The bot has reached the end of {season_episode}".format(season_episode=season_episode))
+                            logging.info("The bot has reached the end of {season_episode}".format(season_episode=season_episode))
+                            webhook = DiscordWebhook(url=webhookURL, content='<@{userID}> The bot has reached the end of {season_episode}. Next episode: {nextEP}'.format(userID=userID, season_episode=season_episode, nextEP=nextEP))
+                            response = webhook.execute()
+                        
+                        #Sleep for specified time before posting next frame
+                        sleep(tweetDelay)
+                    #Twitter API error handling
+                    except tweepy.errors.TwitterServerError as tweepyServerError:
+                        #Twitter API likes to spam 503 Service Unavailable when everytime but one time I've checked, the tweet was posted successfully which causes a double post when the cycle continues
+                        #This if statement prevents double posts by restarting the bot after saving frame progress but could miss frames if the tweet wasn't posted. Personally I'll tolerate one missed frame over 50 double posts.
+                        if tweepyServerError.response.status_code == 503:
+                            logging.exception('Twitter API: 503 Service Unavailable on frame {currentFrame}'.format(currentFrame=currentFrame))
+                            print('Twitter API: 503 Service Unavailable on frame {currentFrame}'.format(currentFrame=currentFrame))
+                            
+                            #Discord webhook to inform about 503 error
+                            webhook = DiscordWebhook(url=webhookURL, content='<@{userID}> Twitter API: 503 Service Unavailable on frame {currentFrame}'.format(userID=userID, currentFrame=currentFrame))
+                            response = webhook.execute()
+                            
+                            #Continuation of after frame posting cycle
+                            #Calculate remaining frames left in episode
+                            remainingFrames = totalFrames - currentFrame
+                        
+                            #Save current frame progress to config
+                            print("Saving progress to config")
+                            currentFrame = i + 1
+                            config_data['currentFrame'] = '{currentFrame}'.format(currentFrame=currentFrame)
+                            with open(config_path, 'w') as f:
+                                f.write(json.dumps(config_data, indent=4, sort_keys=False))
+                        
+                            #Calculate total seconds left until the episode finishes so it can be used in the conversion defined above
+                            secondsleft = remainingFrames * tweetDelay
+                            get_time_remaining(secondsleft)
+                        
+                            #If statement to inform through Discord that the episode has ended during a 503 error which could be problematic, unlikely to occur at the same time as a 503 error but possible
+                            if remainingFrames == 0:
+                                print("The bot has reached the end of {season_episode}".format(season_episode=season_episode))
+                                logging.info("The bot has reached the end of {season_episode}".format(season_episode=season_episode))
+                                sleep(2)
+                                webhook = DiscordWebhook(url=webhookURL, content='<@{userID}> The bot has reached the end of {season_episode}. Next episode: {nextEP}. This occurred during a Twitter API 503 error'.format(userID=userID, season_episode=season_episode, nextEP=nextEP))
+                                response = webhook.execute()
+                                sleep(tweetDelay)
+                                continue
+                            
+                            #Sleep for specified time before posting next frame
+                            sleep(tweetDelay)
+                            
+                            #Restart script after 503 error
+                            print("Restarting bot...")
+                            logging.info("Restarting bot...")
+                            os.execv(sys.executable, ['python3'] + sys.argv)
+                        #Store traceback exception and print to console and log
+                        print_error()
+                        
+                        #Discord webhook to inform about error, second message can fail if error is longer than maximum Discord message character count of 2000
+                        send_discord_webhook()
+                        
+                        #Sleep for specified time before retrying
+                        sleep(tweetDelay)
+                        continue
+                    #Connection lost handling
+                    except socket.gaierror:
+                        #Store traceback exception and print to console and log
+                        print_error()
+                        
+                        #Sleep for specified time before retrying
+                        sleep(tweetDelay)
+                        
+                        #Discord webhook to inform about error, will fail if connection is still down which is why we run this after the sleep period
+                        webhook = DiscordWebhook(url=webhookURL, content='<@{userID}> socket.gaierror exception has occurred with the Twitter bot and it might have stopped posting! Current frame: {currentFrame}'.format(userID=userID, currentFrame=currentFrame))
+                        response = webhook.execute()
+                        continue
+                    #General error handling
+                    except Exception:
+                        #Store traceback exception and print to console and log
+                        print_error()
+                        
+                        #Discord webhook to inform about error, second message can fail if error is longer than maximum Discord message character count of 2000
+                        send_discord_webhook()
+                        
+                        #Sleep for specified time before retrying
+                        sleep(tweetDelay)
+                        continue
+                    else:
+                        break
+
+            #Reset currentFrame to 1 for next episode
+            currentFrame = 1
+            config_data['currentFrame'] = '{currentFrame}'.format(currentFrame=currentFrame)
+            with open(config_path, 'w') as f:
+                f.write(json.dumps(config_data, indent=4, sort_keys=False))
+            
+            #Add 1 to current episode number and save it
+            currentEPnum = j + 1
+            config_data['currentEPnum'] = '{currentEPnum}'.format(currentEPnum=currentEPnum)
+            with open(config_path, 'w') as f:
+                f.write(json.dumps(config_data, indent=4, sort_keys=False))
+            
+            #Writes next season_episode for use in tweet text and saves it
+            temp_season_slash_episode = allEPs[currentEPnum-1]
+            season_slash_episode = temp_season_slash_episode.replace("\n", "")
+            season_episode = season_slash_episode.replace("/", " ")
+            config_data['currentEP'] = '{season_episode}'.format(season_episode=season_episode)
+            with open(config_path, 'w') as f:
+                f.write(json.dumps(config_data, indent=4, sort_keys=False))
+        except Exception:
+            #Store traceback exception and print to console and log
+            print_error()
+            
+            #Discord webhook to inform about error, second message can fail if error is longer than maximum Discord message character count of 2000
+            send_discord_webhook()
+            
+            #Sleep for specified time before retrying
+            sleep(tweetDelay)
+            continue
+        else:
+            break
+
+#Store traceback exception and print to console and log
+error = traceback.format_exc()
+print(error)
+logging.exception("If you are reading this then the bot has likely finished, if so congrats! if not an error occurred.")
+print("If you are reading this then the bot has likely finished, if so congrats! if not an error occurred.")
+
+#Discord webhook to inform either about bot completion or error, second message can fail if error is longer than maximum Discord message character count of 2000
+webhook = DiscordWebhook(url=webhookURL, content='<@{userID}> If you are reading this then the bot has likely finished, if so congrats! if not an error occurred.'.format(userID=userID))
+response = webhook.execute()
+sleep(2)
+webhook = DiscordWebhook(url=webhookURL, content='{error}'.format(error=error))
+response = webhook.execute()
+
+#Terminate script
+sys.exit(1)
